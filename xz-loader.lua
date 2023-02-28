ded20a9d46bf09ac780b52b9529e7c0040b6d2aa
Cremi <89044831+CremiDevelopment@users.noreply.github.com>
2023-02-28 20:52:11 +0200

    Create xz-loader.lua

---
+++ xz-loader.lua
@@ -0,0 +1,12 @@
+--[[
+    This script created by xz#8325
+    XZ's scripts server: https://discord.gg/7sUzjKvjNY
+--]]
+
+local gmD = game.GameId
+
+if gmD == '3256689155' then
+    loadstring(game:HttpGet'https://raw.githubusercontent.com/xzqecc/Script/main/Sakura%20Stand.lua')()
+else if gmD == '1511883870' then
+    loadstring(game:HttpGet'https://raw.githubusercontent.com/xzqecc/Script/main/xzqec_changer_new')()
+end
