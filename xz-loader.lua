b238d21e1eb4391684bf1657efc3081b5a90b78b
Cremi <89044831+CremiDevelopment@users.noreply.github.com>
2023-02-28 22:57:29 +0200

    Update xz-loader.lua

--- xz-loader.lua
+++ xz-loader.lua
@@ -1,6 +1,11 @@
 --[[
     This script created by xz#8325
     XZ's scripts server: https://discord.gg/7sUzjKvjNY
+
+    Support games:
+        ~ Sakura Stand: https://www.roblox.com/games/8534845015/
+        ~ Shindo Life: https://www.roblox.com/games/4616652839/
+        ~ AUT (A Universal Time): https://www.roblox.com/games/5130598377
 --]]
 
 local gmD = game.GameId
@@ -9,4 +14,6 @@ if gmD == '3256689155' then
     loadstring(game:HttpGet'https://raw.githubusercontent.com/xzqecc/Script/main/Sakura%20Stand.lua')()
 else if gmD == '1511883870' then
     loadstring(game:HttpGet'https://raw.githubusercontent.com/xzqecc/Script/main/xzqec_changer_new')()
+else if gmD == '1782832995' then
+    loadstring(game:HttpGet'https://raw.githubusercontent.com/xzqecc/Script/main/aut.lua')()
 end
